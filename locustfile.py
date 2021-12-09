ef971cc51fa32616214e6cdb7dfacb87fd8bfd6b
Simon Östling <ostling.simon@gmail.com>
2021-12-09 15:46:06 +0100

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,7 +1,7 @@
 from locust import HttpUser, between, task
 class WebsiteUser(HttpUser):
     wait_time = between(5, 15)
-    host = "https://google.com"
+    host = "https://load-demo.app01.arrowdemo.xyz"
     @task
     def index(self):
         self.client.get("/")
